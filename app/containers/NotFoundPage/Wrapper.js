ba631bf69b12aed6a7cd54fb93ad33315ea87891
bhefty <billhefty@gmail.com>
2018-01-21 12:36:56 -0600

    add styling to 404 page

---
+++ app/containers/NotFoundPage/Wrapper.js
@@ -0,0 +1,37 @@
+import styled from 'styled-components'
+
+export default styled.div`
+  background-color: #3D3F47;
+  color: #fff;
+  height: calc(100vh - 172px);
+  width: 100vw;
+  display: grid;
+  letter-spacing: 2px;
+  font-size: 2em;
+  position: absolute;
+  left: 0;
+  
+  .container {
+    margin: auto;
+  }
+
+  .four04-comment {
+    color: #4F8C4B;
+  }
+
+  .four04-if {
+    color: #d65562;
+  }
+
+  .four04-not {
+    color: #2796ec;
+  }
+
+  .four04-exception {
+    color: #C684C1;
+  }
+
+  .four04-flip {
+    color: #CF916D;
+  }
+`
