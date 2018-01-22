3a0269092389274c7458375b26dbfb0a2288499c
bhefty <billhefty@gmail.com>
2018-01-21 21:54:56 -0600

    add styled-components theme provider

--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Helmet from 'react-helmet'
-import styled from 'styled-components'
+import styled, { ThemeProvider } from 'styled-components'
 import { Switch, Route } from 'react-router-dom'
 
 import Header from 'components/Header'
@@ -9,33 +9,44 @@ import HomePage from 'containers/HomePage/Loadable'
 import FeaturePage from 'containers/FeaturePage/Loadable'
 import NotFoundPage from 'containers/NotFoundPage/Loadable'
 
+const theme = {
+  primary: '#434C5E',
+  lightShade: '#E9E6D9',
+  lightAccent: '#77938D',
+  darkAccent: '#5472A1',
+  darkShade: '#1B1720',
+  whiteMain: '#FAFAFA'
+}
+
 const AppWrapper = styled.div`
   max-width: calc(768px + 16px * 2);
   margin: 0 auto;
   display: flex;
-  min-height: calc(100vh - 172px); // min height for App wrapper should account for nav and footer
+  min-height: calc(100vh - 160px); // min height for App wrapper should account for nav and footer
   padding: 0 16px;
   flex-direction: column;
 `
 
 export default function App () {
   return (
-    <div>
-      <Helmet
-        titleTemplate='%s - React.js Boilerplate'
-        defaultTitle='React.js Boilerplate'
-      >
-        <meta name='description' content='A React.js Boierlplate application with Redux' />
-      </Helmet>
-      <Header />
-      <AppWrapper>
-        <Switch>
-          <Route exact path='/' component={HomePage} />
-          <Route path='/features' component={FeaturePage} />
-          <Route path='' component={NotFoundPage} />
-        </Switch>
-      </AppWrapper>
-      <Footer />
-    </div>
+    <ThemeProvider theme={theme}>
+      <div>
+        <Helmet
+          titleTemplate='%s - React.js Boilerplate'
+          defaultTitle='React.js Boilerplate'
+        >
+          <meta name='description' content='A React.js Boierlplate application with Redux' />
+        </Helmet>
+        <Header />
+        <AppWrapper>
+          <Switch>
+            <Route exact path='/' component={HomePage} />
+            <Route path='/features' component={FeaturePage} />
+            <Route path='' component={NotFoundPage} />
+          </Switch>
+        </AppWrapper>
+        <Footer />
+      </div>
+    </ThemeProvider>
   )
 }
