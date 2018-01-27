6541418fad758503e5e55c822b2e26b7ce291bda
bhefty <billhefty@gmail.com>
2018-01-27 12:06:06 -0600

    add responsive drawer toggle

--- app/components/Header/Wrapper.js
+++ app/components/Header/Wrapper.js
@@ -2,7 +2,7 @@ import styled from 'styled-components'
 
 export default styled.header`
   background-color: ${props => props.theme.primary};
-  height: 48px;
+  height: ${props => props.expanded ? '100%' : '48px'};
   box-shadow: 0 2px 4px rgba(0,0,0,0.18), 0 2px 3px rgba(0,0,0,0.26);
   white-space: nowrap;
   overflow-x: auto;
@@ -12,55 +12,117 @@ export default styled.header`
   top: 0;
   z-index: 11101;
   
-  padding: 0 10px;
-  display: grid;
-  grid-gap: 10px;
-  grid-template-columns: 48px 1fr;
-  grid-auto-flow: column;
-  align-content: center;
+  /* padding: 0 10px; */
 
-  .header__logo {
-    img {
-      width: 100%;
-    }
-  }
-
-  .header__brand {
-    padding 0 10px;
-    justify-self: start;
-    font-size: 1.5em;
-
-    &:hover {
-      background-color: ${props => props.theme.lightAccent};
-    }
-  }
-
-  .header__links {
-    justify-self: end;
-
+  .header {
     display: grid;
-    grid-template-columns: 1fr;
-    grid-auto-flow: column;
     grid-gap: 10px;
+    grid-template-columns: 48px 1fr;
+    grid-auto-flow: column;
+    align-content: center;
 
-    span {
-      padding: 0 10px;
+    .header__logo {
+      img {
+        width: 100%;
+      }
     }
 
-    a:hover {
-      background-color: ${props => props.theme.lightAccent};
+    .header__brand {
+      padding 0 10px;
+      justify-self: start;
+      font-size: 1.5em;
+
+      &:hover {
+        background-color: ${props => props.theme.lightAccent};
+      }
+    }
+
+    .header__links {
+      justify-self: end;
+
+      display: grid;
+      grid-template-columns: 1fr;
+      grid-auto-flow: column;
+      grid-gap: 10px;
+
+      span {
+        padding: 0 10px;
+      }
+
+      a:hover {
+        background-color: ${props => props.theme.lightAccent};
+      }
+    }
+
+    .header__link {
+      display: grid;
+      span {
+        margin: auto;
+      }
+    }
+
+    a, a:visited, button {
+      color: ${props => props.theme.whiteMain};
+      text-decoration: none;
+    }
+
+    button {
+      border: none;
+      font-size: 1.5em;
+    }
+
+    [aria-controls="menu-list"] {
+      display: none;
     }
   }
 
-  .header__link {
-    display: grid;
-    span {
-      margin: auto;
-    }
-  }
+  @media (max-width: 700px) {
+    .header {
+      grid-template-columns: 48px 1fr 48px;
+      grid-auto-flow: row;
+      grid-template-rows: 1fr;
+      /* grid-auto-rows: 50px; */
+      grid-gap: 0;
 
-  a, a:visited {
-    color: ${props => props.theme.whiteMain};
-    text-decoration: none;
+      [aria-controls="menu-list"] {
+        display: block;
+      }
+    }
+    
+    .header__links {
+      max-height: 0;
+      max-width: 0;
+      overflow: hidden;
+      transform: translateY(-20%);
+      transition: 0.3s cubic-bezier(.2, 0, 0, 1.6);
+    }
+
+    [aria-expanded='true'] ~ .header__links {
+      max-height: 100%;
+      max-width: 100%;
+      grid-column: 1 / -1;
+      background: ${props => props.theme.darkAccent};
+
+      display: grid;
+      grid-auto-flow: row;
+      justify-self: stretch;
+      transform: translateY(0%);
+
+      .header__link {
+        padding: 0.5em 0;
+      }
+    }
+
+    [aria-expanded='false'] .close {
+      display: none;
+    }
+
+    [aria-expanded='true'] .close {
+      display: inline-block;
+    }
+
+    [aria-expanded='true'] .open {
+      display: none;
+    }
   }
 `
--- app/components/Header/index.js
+++ app/components/Header/index.js
@@ -1,18 +1,37 @@
-import React, { PureComponent } from 'react'
+import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 
 import Img from 'components/Img'
 import logo from './logo_white.png'
 import Wrapper from './Wrapper'
 
-class Header extends PureComponent {
+class Header extends Component {
+  constructor () {
+    super()
+    this.toggleNav = this.toggleNav.bind(this)
+
+    this.state = {
+      expanded: false
+    }
+  }
+
+  toggleNav () {
+    this.setState({ expanded: !this.state.expanded })
+  }
+
   render () {
     return (
-      <Wrapper>
-        <Link to='/' className='header__logo'><Img src={logo} alt='React Redux Boilerplate' /></Link>
-        <Link to='/' className='header__link header__brand'><span>React Redux Boilerplate</span></Link>
-        <div className='header__links'>
-          <Link to='/features' className='header__link'><span>Features</span></Link>
+      <Wrapper expanded={this.state.expanded}>
+        <div className='header'>
+          <Link to='/' className='header__logo'><Img src={logo} alt='React Redux Boilerplate' /></Link>
+          <Link to='/' className='header__link header__brand'><span>React Redux Boilerplate</span></Link>
+          <button aria-expanded={this.state.expanded} aria-controls='menu-list' onClick={this.toggleNav}>
+            <span className='open'>☰</span>
+            <span className='close'>×</span>
+          </button>
+          <div id='menu-list' className='header__links'>
+            <Link to='/features' className='header__link'><span>Features</span></Link>
+          </div>
         </div>
       </Wrapper>
     )
