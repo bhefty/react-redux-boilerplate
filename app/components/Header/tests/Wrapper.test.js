a090efa2b2b08d2bde6abd7c79c0fe390cb9f53b
bhefty <billhefty@gmail.com>
2018-01-27 12:05:41 -0600

    update tests

--- app/components/Header/tests/Wrapper.test.js
+++ app/components/Header/tests/Wrapper.test.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { shallow } from 'enzyme'
+import 'jest-styled-components'
 
 import Wrapper from '../Wrapper'
 
@@ -23,5 +24,15 @@ describe('<Wrapper />', () => {
   it('should not adopt an invalid attribute', () => {
     const renderedComponent = shallow(<Wrapper attribute={'test'} />)
     expect(renderedComponent.prop('attribute')).toBeUndefined()
+  })
+
+  it('should render with a height of 48px if props.expanded is false', () => {
+    const renderedComponent = shallow(<Wrapper expanded={false} />)
+    expect(renderedComponent).toHaveStyleRule('height', '48px')
+  })
+
+  it('should render with a height of 100% if props.expanded is false', () => {
+    const renderedComponent = shallow(<Wrapper expanded />)
+    expect(renderedComponent).toHaveStyleRule('height', '100%')
   })
 })
--- app/components/Header/tests/index.test.js
+++ app/components/Header/tests/index.test.js
@@ -17,4 +17,26 @@ describe('<Header />', () => {
     )
     expect(renderedComponent.find('Link').length).toEqual(3)
   })
+
+  describe('toggleNav', () => {
+    it('should toggle state of expanded from false to true', () => {
+      const renderedComponent = shallow(
+        <Header />
+      )
+      expect(renderedComponent.state('expanded')).toBe(false)
+      renderedComponent.instance().toggleNav()
+      expect(renderedComponent.state('expanded')).toBe(true)
+    })
+
+    it('should toggle state of expanded from true to false', () => {
+      const renderedComponent = shallow(
+        <Header />
+      )
+      expect(renderedComponent.state('expanded')).toBe(false)
+      renderedComponent.setState({ expanded: true })
+      expect(renderedComponent.state('expanded')).toBe(true)
+      renderedComponent.instance().toggleNav()
+      expect(renderedComponent.state('expanded')).toBe(false)
+    })
+  })
 })
